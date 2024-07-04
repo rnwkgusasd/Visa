018619186e892e95f192306ef94f9e41de3a6340
JaHyun_K <rnwkgusasd@naver.com>
2024-07-04 16:37:43 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Vsia;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Visa.Visa visa = new Visa.Visa();
+            string[] resourceList = visa.FindResource("DSA");
+            bool isOk = visa.Open(resourceList);
+            Console.WriteLine();
+        }
+    }
+}
